168f9ecf32c2ae5e04d5179d487143ef126d1777
DSS Network <dssnetwork@iMac-de-DSS-4.local>
2017-04-03 19:33:45 +0200

    rendered hit and fail's counter and random questions

--- Quiz/js/script.js
+++ Quiz/js/script.js
@@ -2,14 +2,14 @@ class Quiz{
     constructor(props){
         this.props = props;
         
-        this.quiz = document.getElementById("quiz")
+        this.quiz = document.getElementById("quiz");
         this.counterHits = 0;
         this.counterFails = 0;
-        this.renderQuestions = this.renderQuestions.bind(this);
-        
+        this.renderQuestionsAndAnswers = this.renderQuestionsAndAnswers.bind(this);
     }
+
     //TODO
-    renderQuestions(){
+    renderQuestionsAndAnswers(){
         var questions = this.props.questions;
              
         for(var i=0; i < questions.length; i++){
@@ -36,35 +36,96 @@ class Quiz{
  
     checkAnswer(answer,questionPos, answerPos) {
         var answerProps = this.props.answers[questionPos][answerPos];
-        var answerClicked = this.props.answers[questionPos][this.props.correctchoice[questionPos]];
+        var answerClicked = this.props.answers[questionPos][this.props.correctChoice[questionPos]];
         
         if(answer.className == "answer"){
             answer.className += "-toggled";
             if((answerProps == answerClicked) && this.counterHits<this.props.hits){ //Hit
-                
+                var counterH = ++this.counterHits
+                this.updateCounter()
                 this.props.onHit();
-                if(++this.counterHits==this.props.hits){
+                if(counterH==this.props.hits){
                     this.props.onFinishHit();
+                    this.counterFails = this.props.fails;
                 }
 
             } else if(this.counterFails<this.props.fails) { // Fail
+                var counterF = ++this.counterFails
+                this.updateCounter()
                 this.props.onFail();
-                if (++this.counterFails==this.props.fails){
+                if (counterF==this.props.fails){
                     this.props.onFinishFail();
+                    this.counterHits = this.props.hits;
                 }
             }
         }
     }
-      
+    
+    updateCounter(){
+        var counters = document.getElementById("counters");
+        counters.innerHTML = "Aciertos: "+this.counterHits+"<br>"+"Fallos: "+this.counterFails;
+    }
+    
+    renderCounter(){
+        var counters = document.createElement("div");
+            counters.id = "counters";
+            counters.className = "counters";
+        this.quiz.appendChild(counters);
+        
+        this.updateCounter();
+        
+    }
+    
+    randomize() {
+        //TODO Sort randomly question, then sorted answers and correctChoice according to question.
+        var mask = createMask(this.props.questions);
+        sortArray(mask, this.props.questions);
+        sortArray(mask,this.props.answers);
+        sortArray(mask, this.props.correctChoice)
+    }
    
     render() {
-        this.renderQuestions();
+        this.props.random ? this.randomize() : null;  //if(this.props.random) this.randomize()
+        this.renderQuestionsAndAnswers();
+        this.renderCounter();
+    }
+    
+    clean(){
+        //TODO clean quiz
+        
     }
     
     abort() {
-        console.log(this.props.correctchoice[1]);
+        console.log(this.props.correctChoice[1]);
     }
     
-           
+}
 
+function sortArray(mask, array){ // new positions: [2,3,0,1]
+    //TODO 
+    
+}
+
+function createMask(array) {
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
+  var mask = []
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+    return mask;
+  // return [2,3,0,1]
+  // return array;
 }
